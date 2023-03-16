11341eb15c5aa46742d7099b1b949cce833b2761
maurisq12 <maurisq@hotmail.com>
2023-03-16 16:41:59 -0600

    cambio qr

--- Controllers/reservacionesEstudiante.cs
+++ Controllers/reservacionesEstudiante.cs
@@ -14,25 +14,24 @@ public class Reservas : Controller
 
     //SET: /Cubi/
     public IActionResult misReservaciones(){
-        
-
-
-        
-
+        Models.CodigosQR miQR = new CodigosQR();
+        string elcodigo64 = miQR.crearCodigo();
+        string codigoListo = "data:image/png;charset=utf-8;base64,"+elcodigo64;
+        ViewData["Imagen"]= codigoListo;
         return View();
     }
 
-    public void confirmando(){
+    public string confirmando(){
         Models.CodigosQR miQR = new CodigosQR();
         string elcodigo64 = miQR.crearCodigo();
         string codigoListo = "data:image/png;charset=utf-8;base64,"+elcodigo64;
         ViewData["Imagen"]= codigoListo;
         Models.Pdfs nuevoPDF = new Pdfs();
         nuevoPDF.crear();
-        
         //envio de correo con confirmación
         Models.Correos servCorreo = new Correos();
         servCorreo.enviarCorreo(codigoListo);
+        return "listo";
     }
 
 }
